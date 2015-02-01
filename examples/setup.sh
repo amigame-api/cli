7d6be07e62010426550182e2561ce2ead034c98f
Makis Tracend <makis.tracend@gmail.com>
2015-01-31 19:22:47 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Amigame CLI
+
+# 1. Install the main executable
+npm install -g amigame-cli
+
+#2. Setup amigame (only needed once)
+amigame setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+amigame -h
